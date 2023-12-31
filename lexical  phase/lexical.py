c712a39c2b03fbfcf83a131ed6d6619ad622ac33
mohamedabdelaziz7 <137700167+mohamedabdelaziz7@users.noreply.github.com>
2023-12-31 02:14:15 +0200

    Add files via upload

---
+++ lexical  phase/lexical.py
@@ -0,0 +1,100 @@
+import re
+
+# Token types
+INTEGER = 'INTEGER'
+PLUS = 'PLUS'
+MINUS = 'MINUS'
+MULTIPLY = 'MULTIPLY'
+DIVIDE = 'DIVIDE'
+LPAREN = 'LPAREN'
+RPAREN = 'RPAREN'
+EOF = 'EOF'
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return f'Token({self.type}, {repr(self.value)})'
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    @staticmethod
+    def error():
+        raise Exception('Invalid character')
+
+    def advance(self):
+        self.pos += 1
+        if self.pos < len(self.text):
+            self.current_char = self.text[self.pos]
+        else:
+            self.current_char = None
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return result
+
+    def get_next_token(self):
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(MULTIPLY, '*')
+
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIVIDE, '/')
+
+            if self.current_char == '(':
+                self.advance()
+                return Token(LPAREN, '(')
+
+            if self.current_char == ')':
+                self.advance()
+                return Token(RPAREN, ')')
+
+            self.error()
+
+        return Token(EOF, None)
+
+
+# Example Usage
+def main():
+    source_code = "(3 + 4) * 5"
+
+    lexer = Lexer(source_code)
+
+    while True:
+        token = lexer.get_next_token()
+        if token.type == EOF:
+            break
+        print(token)
+
+if __name__ == "__main__":
+    main()
